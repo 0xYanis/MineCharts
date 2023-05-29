fa136c8a6b10f7b89e3dd2ea783b6be021656b97
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-05-29 15:49:24 +0300

    Initial Commit

---
+++ MineCharts/MineChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MineChartsApp.swift
+//  MineCharts
+//
+//  Created by Yan Rybkin on 29.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct MineChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
