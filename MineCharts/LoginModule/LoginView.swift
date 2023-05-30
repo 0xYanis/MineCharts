ab7dfb8a7204e21731df543d45550b20a7a5bcef
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-05-30 12:21:28 +0300

    Configure sheet view

--- MineCharts/LoginModule/LoginView.swift
+++ MineCharts/LoginModule/LoginView.swift
@@ -11,6 +11,9 @@ struct LoginView: View {
     
     @StateObject var viewModel: LoginViewModel = LoginViewModel()
     
+    @State private var isShowingModal = false
+    @State private var isEditing = false
+    
     var body: some View {
         ZStack {
             Color.backColor
@@ -22,49 +25,66 @@ struct LoginView: View {
             
             VStack {
                 HStack {
-                    Text("Welcome Back !")
+                    Text("Welcome\nBack !")
                         .foregroundColor(.white)
-                        .font(.system(size: 50, weight: .heavy, design: .default))
-                        .frame(width: 300)
+                        .font(.system(size: 46, weight: .heavy, design: .default))
+                        .multilineTextAlignment(.leading)
+                    
                     Spacer()
-                }
+                    
+                }.padding(.horizontal, 30)
                 
                 Spacer()
                 
                 VStack {
                     HStack {
-                        TextField("Entry Your wallet hash...", text: $viewModel.walletHash)
+                        TextField(
+                            "Entry your wallet hash...",
+                            text: $viewModel.walletHash
+                        )
                     }
                     .padding(.horizontal, 15)
-                    .font(.system(size: 22))
+                    .font(.system(size: 20))
                     .frame(height: 50)
                     .cornerRadius(25)
                     .overlay(
                         RoundedRectangle(cornerRadius: 25)
                             .stroke(Color.orange, lineWidth: 2)
                     )
-                    .padding(.horizontal)
+                    .padding(.horizontal, 30)
                 }
                 
                 Spacer()
                 
                 HStack {
                     Button(action: {
-                        
+                        isEditing.toggle()
+                        hideKeyboard()
+                        isShowingModal.toggle()
                     }, label: {
                         Text("What is it?")
                             .font(.system(size: 20, weight: .bold, design: .default))
                             .foregroundColor(.orange)
                             .padding()
-                    })
+                        
+                    }).sheet(isPresented: $isShowingModal) {
+                        LoginInfoView()
+                            .presentationDetents([.fraction(0.6)])
+                    }
+                    
                     
                     Spacer()
+                    
                     
                     Button(action: {
                         
                     }, label: {
-                        Text("--------->")
-                            .font(.system(size: 20, weight: .bold, design: .default))
+                        Text("Let's go!")
+                            .font(.system(
+                                size: 20,
+                                weight: .bold,
+                                design: .default)
+                            )
                             .foregroundColor(.white)
                             .padding(.horizontal)
                             .frame(height: 50)
@@ -72,22 +92,36 @@ struct LoginView: View {
                             .cornerRadius(25)
                     })
                 }
-                .padding()
+                .padding(.horizontal, 30)
                 
                 Spacer()
                 
                 VStack {
                     Text("MineCharts. Version: 0.1")
-                        .foregroundColor(.gray)
                     Text("0xYanis. ITMO University")
-                        .foregroundColor(.gray)
                 }
+                .foregroundColor(.gray)
+                .ignoresSafeArea(.keyboard)
                 
             }
-            .padding(.top, 200)
+            .padding(.top, 180)
+        }
+        .onTapGesture {
+            isEditing.toggle()
+            hideKeyboard()
         }
         
-        
+    }
+}
+
+private extension LoginView {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(
+            #selector(UIResponder.resignFirstResponder),
+            to: nil,
+            from: nil,
+            for: nil
+        )
     }
 }
 
---
+++ MineCharts/LoginModule/Subviews/LoginInfoView.swift
@@ -0,0 +1,59 @@
+//
+//  LoginInfoView.swift
+//  MineCharts
+//
+//  Created by Yan Rybkin on 29.05.2023.
+//
+
+import SwiftUI
+
+struct LoginInfoView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            VStack {
+                HStack {
+                    Text("Описание приложения")
+                        .foregroundColor(.white)
+                        .font(.system(size: 24, weight: .heavy, design: .default))
+                    
+                    Spacer()
+                }
+                .padding()
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Мы предлагаем сервис, который агрегирует данные майнинга различных криптовалют с разных пулов.")
+                    
+                    Text("Наше приложение предоставляет пользователям доступ к актуальной информации о доходности майнинга, текущей сложности сети, количестве добытых блоков и наградах.")
+                    
+                    Text("Данное прилоежение является проектом магистерской диссертационной работы.")
+                }
+                .foregroundColor(.white)
+                .font(.system(size: 16))
+                .padding()
+                
+                Button(action: {
+                    dismiss()
+                }, label: {
+                    Text("Понятно")
+                        .foregroundColor(.orange)
+                        .font(.system(size: 20, weight: .heavy, design: .default))
+                })
+                
+                Spacer()
+            }
+            
+        }
+    }
+}
+
+struct LoginInfo_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginInfoView()
+            .preferredColorScheme(.dark)
+    }
+}
