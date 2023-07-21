9c6e4316dfdf55e0b63a20b718b4880d2e8d792c
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-07-21 23:06:23 +0300

    Refactoring

--- MineCharts/LoginModule/Subviews/LoginInfoView.swift
+++ MineCharts/LoginModule/Subviews/LoginInfoView.swift
@@ -7,6 +7,12 @@
 
 import SwiftUI
 
+fileprivate enum LoginInfo : String {
+    case first = "Мы предлагаем сервис, который агрегирует данные майнинга различных криптовалют с разных пулов."
+    case second = "Наше приложение предоставляет пользователям доступ к актуальной информации о доходности майнинга, текущей сложности сети, количестве добытых блоков и наградах."
+    case third = "Данное прилоежение является проектом магистерской диссертационной работы."
+}
+
 struct LoginInfoView: View {
     
     @Environment(\.dismiss) var dismiss
@@ -14,28 +20,8 @@ struct LoginInfoView: View {
     var body: some View {
         NavigationStack {
             ZStack {
-                Color.black
-                    .ignoresSafeArea()
-                VStack {
-                    VStack(alignment: .leading, spacing: 20) {
-                        Text("Мы предлагаем сервис, который агрегирует данные майнинга различных криптовалют с разных пулов.")
-                        
-                        Text("Наше приложение предоставляет пользователям доступ к актуальной информации о доходности майнинга, текущей сложности сети, количестве добытых блоков и наградах.")
-                        
-                        Text("Данное прилоежение является проектом магистерской диссертационной работы.")
-                    }
-                    .foregroundColor(.white)
-                    .padding()
-                    
-                    Spacer()
-                    
-                    VStack {
-                        Text("MineCharts. Version: 0.1")
-                        Text("0xYanis. ITMO University")
-                    }
-                    .foregroundColor(.gray)
-                }
-                
+                Color.black.ignoresSafeArea()
+                loginScreen()
             }
             
             .toolbar {
@@ -53,6 +39,29 @@ struct LoginInfoView: View {
             .navigationBarTitleDisplayMode(.inline)
         }
     }
+    
+    @ViewBuilder
+    private func loginScreen() -> some View {
+        VStack {
+            VStack(alignment: .leading, spacing: 20) {
+                Text(LoginInfo.first.rawValue)
+                Text(LoginInfo.second.rawValue)
+                Text(LoginInfo.third.rawValue)
+            }
+            .foregroundColor(.white)
+            .padding()
+            
+            Spacer()
+            
+            VStack {
+                Text("MineCharts. Version: 0.1")
+                Text("0xYanis. ITMO University")
+            }
+            .foregroundColor(.gray)
+        }
+    }
+    
+    
 }
 
 struct LoginInfo_Previews: PreviewProvider {
